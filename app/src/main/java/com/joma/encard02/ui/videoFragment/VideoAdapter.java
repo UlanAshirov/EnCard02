ff1a1fdeae982d750b5e0f9ff59bb9973246b048
ProgramTogether <hchcdgddh@gmail.com>
2022-03-09 20:32:27 +0600

    Poputalsa sdelat po MVVM and Video

---
+++ app/src/main/java/com/joma/encard02/ui/videoFragment/VideoAdapter.java
@@ -0,0 +1,58 @@
+package com.joma.encard02.ui.videoFragment;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.joma.encard02.data.videoModel.VideoHit;
+import com.joma.encard02.data.videoModel.Videos;
+import com.joma.encard02.databinding.ItemVideoBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.http.Url;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHolder> {
+    private ItemVideoBinding binding;
+    private List<VideoHit> videoHits = new ArrayList<>();
+
+    public void setVideoHits(List<VideoHit> videoHits) {
+        this.videoHits = videoHits;
+    }
+
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ItemVideoBinding.inflate(LayoutInflater.from(parent.getContext()),
+                parent, false);
+        return new VideoViewHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoAdapter.VideoViewHolder holder, int position) {
+        holder.onBindVideo(videoHits.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoHits.size();
+    }
+
+    public class VideoViewHolder extends RecyclerView.ViewHolder {
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void onBindVideo(VideoHit videoHit) {
+           binding.itemImgVideo.videoUrl(videoHit.getVideos().getSmall().getUrl()).enableAutoStart();
+            binding.tvItemTags.setText(videoHit.getTags());
+        }
+    }
+}
